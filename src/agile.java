56814111fa794cdf4e7d3131dd6ddbef10b5cbd6
“omar2432” <omar.ahmed2432@gmail.com>
2020-08-22 20:23:24 +0200

    Addition func

---
+++ src/agile.java
@@ -0,0 +1,17 @@
+public class agile {
+
+
+        public static void main(String[] args) {
+            System.out.println("Hello World!");
+            add();
+        }
+
+
+    public static void add() {
+            int x= 5;
+        int y= 7;
+        int z=x+y;
+        System.out.println(" x + y = "+z);
+    }
+
+}
