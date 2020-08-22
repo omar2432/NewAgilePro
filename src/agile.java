2915bb7cb7e5005ec242f08a5f86d28870d69a7d
omar2432 <omar.ahmed2432@gmail.com>
2020-08-22 20:42:03 +0200

    subtraction func

--- src/agile.java
+++ src/agile.java
@@ -4,6 +4,7 @@ public class agile {
         public static void main(String[] args) {
             System.out.println("Hello World!");
             add();
+            subtract();
         }
 
 
@@ -13,5 +14,14 @@ public class agile {
         int z=x+y;
         System.out.println(" x + y = "+z);
     }
+
+
+    public static void subtract() {
+        int x= 5;
+        int y= 7;
+        int z=x-y;
+        System.out.println(" x - y = "+z);
+    }
+
 
 }
